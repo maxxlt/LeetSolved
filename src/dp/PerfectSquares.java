06863b25cfb78c881820fa12ab52c1dff380ea23
maxxlt <din.chan@mavs.uta.edu>
2020-12-18 00:42:34 -0600

    Perfect Square problem solved with DP

--- src/dp/PerfectSquares.java
+++ src/dp/PerfectSquares.java
@@ -2,7 +2,7 @@ package dp;
 
 public class PerfectSquares {
     public static void main(String[] args) {
-        numSquares(12);
+        numSquares(13);
     }
     private static int numSquares(int n) {
         if(n == 0){
@@ -11,15 +11,26 @@ public class PerfectSquares {
         int[] dp = new int[n+1];
         dp[0] = 0;
         for(int i =1; i <=n;i++){
+            System.out.println("Original");
+            for (int z = 1; z <= n; z++){
+                System.out.print(z + " ");
+            }
+            System.out.println();
+            System.out.println("Number of Perfect Squares");
+            //      13
+            //      v
             dp[i] = i;
             for(int j =1; j*j<=i;j++){
                 dp[i] = Math.min(dp[i], 1+dp[i -j*j]);
+
+                for (int y = 1; y < dp.length; y++){
+                    System.out.print(dp[y] + " ");
+                }
+                System.out.println();
             }
+            System.out.println();
         }
-        System.out.println("Perfect Squares");
-        for (int num : dp){
-            System.out.print(num + " ");
-        }
+
         return dp[n];
     }
 }
