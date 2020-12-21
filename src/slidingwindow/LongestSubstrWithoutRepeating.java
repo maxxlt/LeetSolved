6decf5989b46e0c799a244e1dac7676128a5b3aa
maxthelearner <din.chan@mavs.uta.edu>
2020-12-20 23:08:27 -0600

    Longest substring without repeating problem solved with sliding window, hash set, and 2 pointers

---
+++ src/slidingwindow/LongestSubstrWithoutRepeating.java
@@ -0,0 +1,48 @@
+package slidingwindow;
+
+import java.util.HashSet;
+
+public class LongestSubstrWithoutRepeating {
+    /*
+Given a string s, find the length of the longest substring without repeating characters.
+
+Example 1:
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+
+Example 2:
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+
+Example 3:
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+Example 4:
+Input: s = ""
+Output: 0
+     */
+    public static void main(String[] args) {
+        System.out.println("Longest substring for `abcabcbb` is " + lengthOfLongestSubstring("abcabcbb"));
+    }
+    private static int lengthOfLongestSubstring(String s) {
+        int i = 0, j = 0, max = 0;
+        HashSet<Character> hash_set = new HashSet<>();
+        while (j < s.length()){
+            if (!hash_set.contains(s.charAt(j))){
+                hash_set.add(s.charAt(j));
+                j++;
+                max = Math.max(hash_set.size(), max);
+            }
+            else {
+                hash_set.remove(s.charAt(i));
+                i++;
+            }
+        }
+        return max;
+    }
+}
